aef1e4eba6b6d139c5d7c16615c105122645afaa
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-28 22:13:11 -0500

    Initialization

---
+++ KruskalsAlgorithm/KruskalsAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KruskalsAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
